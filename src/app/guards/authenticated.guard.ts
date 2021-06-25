88f4552dce67a3b22a0ba3ac37dede889475db63
NguyenThanhHuyen <huyen.nguyen@staff.gumiviet.com>
2021-06-25 17:36:01 +0700

    feat(pokemons): implement pokemons-list component

--- src/app/guards/authenticated.guard.ts
+++ src/app/guards/authenticated.guard.ts
@@ -1,40 +1,49 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
-  CanActivate,
-  CanActivateChild,
-  CanLoad,
-  Router
+    CanActivate,
+    CanActivateChild,
+    CanLoad,
+    Router
 } from '@angular/router';
-import { of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import {of} from 'rxjs';
+import {tap} from 'rxjs/operators';
+import {AuthService} from "../services/auth.service";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class AuthenticatedGuard
-  implements CanLoad, CanActivate, CanActivateChild {
-  constructor(private readonly router: Router) {}
+    implements CanLoad, CanActivate, CanActivateChild {
+    constructor(private readonly router: Router,
+                private authService: AuthService) {
+    }
 
-  canLoad() {
-    return this.isAuth$();
-  }
+    canLoad() {
+        return this.isAuth$();
+    }
 
-  canActivate() {
-    return this.isAuth$();
-  }
+    canActivate() {
+        return this.isAuth$();
+    }
 
-  canActivateChild() {
-    return this.isAuth$();
-  }
+    canActivateChild() {
+        return this.isAuth$();
+    }
 
-  private isAuth$() {
-    return of(false).pipe(
-      tap(isAuth => {
-        if (!isAuth) {
-          this.router.navigate(['/not-auth']);
+    private isAuth$() {
+        // return of(false).pipe(
+        //   tap(isAuth => {
+        //     if (this.authService.isLoggedIn()) {
+        //       return true;
+        //     }
+        //     if (!isAuth) {
+        //       this.router.navigate(['/not-auth']);
+        //     }
+        //   })
+        // );
+        if (this.authService.isLoggedIn()) {
+            return true;
+        } else {
+            this.router.navigate(['/not-auth']);
+            return false
         }
-        else {
-          this.router.navigate(['/pokemons']);
-        }
-      })
-    );
-  }
+    }
 }
--- src/app/pokemons/components/list/list.component.ts
+++ src/app/pokemons/components/list/list.component.ts
@@ -1,31 +1,47 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import type { PaginatorState } from '../../../components/paginator/paginator.component';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import type {PaginatorState} from '../../../components/paginator/paginator.component';
+import {BackendService} from "../../../services/backend.service";
+import {Pokemon} from "../../../models/pokemon";
 
 @Component({
-  selector: 'pokemon-list',
-  template: `
-    <paginator
-      [currentPage]="1"
-      [rowsPerPageOptions]="[10, 20, 40, 80]"
-      [rows]="20"
-      [totalRecords]="100"
-      (onPageChange)="onPageChanged($event)"
-    ></paginator>
-    <input
-      type="text"
-      class="w-2/4 p-2 rounded border border-gray-600"
-      placeholder="Filter by pokemon name..."
-      [formControl]="query"
-    />
-    <data-table [isLoading]="false" [data]="[]"></data-table>
-  `,
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'pokemon-list',
+    template: `
+        <paginator
+                [currentPage]="1"
+                [rowsPerPageOptions]="[10, 20, 40, 80]"
+                [rows]="20"
+                [totalRecords]="100"
+                (onPageChange)="onPageChanged($event)"
+        ></paginator>
+        <input
+                type="text"
+                class="w-2/4 p-2 rounded border border-gray-600"
+                placeholder="Filter by pokemon name..."
+                [formControl]="query"
+        />
+        <data-table [isLoading]="false" [data]="data"></data-table>
+    `,
+    // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ListComponent {
-  query = new FormControl('');
+export class ListComponent implements OnInit {
+    data : Pokemon [];
+    query = new FormControl('');
+    onPageChanged(paginatorState: PaginatorState) {
+        console.log(paginatorState);
+    }
 
-  onPageChanged(paginatorState: PaginatorState) {
-    console.log(paginatorState);
-  }
+    constructor(private backendService: BackendService) {
+    }
+
+    ngOnInit() {
+        this.getDataTable()
+    }
+
+    getDataTable() {
+        this.backendService.getPokemons(40, 0).subscribe((res: any) => {
+            this.data = res.results;
+            console.log(res)
+        })
+    }
 }
